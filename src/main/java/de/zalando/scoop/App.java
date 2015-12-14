3c031bd200102b2dc882876bf11dd9e9a845db69
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2015-12-14 12:51:30 +0100

    initial project setup

---
+++ src/main/java/de/zalando/scoop/App.java
@@ -0,0 +1,12 @@
+package de.zalando.scoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
